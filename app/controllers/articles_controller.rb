0dacb8b5ce843079f050c1dab8c8472519f49fb7
Vishy <vishal18593@gmail.com>
2014-06-25 11:34:30 +0530

    First commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,63 @@
+class ArticlesController < ApplicationController
+  
+
+  
+
+  def new 
+    @article = Article.new
+  end
+
+   def index
+   @articles = Article.all
+  end
+
+  def create
+  @article = Article.new(allow_params)
+ 
+  if @article.save
+      redirect_to @article
+  else render 'new'
+  end    
+  end
+ 
+
+
+ 
+def update
+  
+  if Article.update(params[:id],allow_params)
+    redirect_to @article
+  else
+   render 'edit'
+  end
+  end  
+
+
+  def edit
+  @article = Article.find(params[:id])
+  end
+
+  
+
+
+  def show
+      @article = Article.find( params[:id] )
+  end
+
+
+
+
+  def destroy
+     @article = Article.find( params[:id])
+     @article.destroy
+     redirect_to articles_path
+  end
+  
+private
+  def allow_params
+    params.require(:article).permit(:title, :text)
+  end
+#methods defined below a private method will become private  
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '38c20a9174a2d44591778c682920d76ddf11ff0236bf5814ea4992b02dd47c2cafc5a110ee56fe42fc467a239ca2c792fb069019304a1b4eff015895b668bbfe'
