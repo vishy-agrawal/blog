fdd84dd30f3fd1341776e5200a19c46faf61f7cc
Vishy <vishal18593@gmail.com>
2014-06-27 16:58:32 +0530

    sign in complete 2

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,6 +2,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 def google_oauth2
      
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    
  
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140626051845) do
+ActiveRecord::Schema.define(:version => 20140626101111) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20140626051845) do
     t.datetime "updated_at",                             :null => false
     t.string   "provider"
     t.string   "uid"
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
